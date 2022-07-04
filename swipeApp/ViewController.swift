8776bb6465978a93b6847f352b0eaf9da47540a9
kazukiomori <kazuki1110170501@gmail.com>
2022-07-04 23:44:12 +0900

    Initial Commit

---
+++ swipeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swipeApp
+//
+//  Created by Kazuki Omori on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
